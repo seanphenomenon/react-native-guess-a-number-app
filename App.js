ef688212bd85b69083142b4c9a34a5ae0860f543
seanphenomenon <seanvharrell@gmail.com>
2021-02-21 22:47:16 -0800

    added game over logic and screen

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-import Constants from "expo-constants";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import Constants from 'expo-constants';
 
-import Header from "./components/Header";
-import StartGameScreen from "./screens/StartGameScreen";
-import GameScreen from "./screens/GameScreen";
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   //need to manage state here to display different screens and hold on to selected number from startGameScreen.
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  //for below variable, if greather than 0, gameoverhandler executed.
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
   };
 
   // store default components here. Game screen is triggered by onStartGame prop passed down to startGameScreen component.
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  //check if userNumber is true and a number was chosen, then we can change content to game screen.
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
+  //check if userNumber is true and a number was chosen, then we can change content shown to game screen.
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen />;
   }
 
   return (
     <View style={styles.screen}>
-      <Header title="Guess a Number" />
+      <Header title='Guess a Number' />
       {content}
     </View>
   );
@@ -33,16 +45,16 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
 
   inputContainer: {
     flex: 2,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "black",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
     borderWidth: 3,
     borderRadius: 10,
   },
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
-import NumberContainer from "../components/NumberContainer";
-import Card from "../components/Card";
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card';
 
 //app  exclude will never guess users choice on the first try. Looking to get a random number between min and max.
 const generateRandomBetween = (min, max, exclude) => {
@@ -21,13 +21,56 @@ const GameScreen = (props) => {
     generateRandomBetween(1, 100, props.userChoice) // here we want to use the state of random num between min and max, and exclude the users choice because that is the solution.
   );
 
+  const [rounds, setRounds] = useState(0);
+  const currentLow = useRef(1); ///stored value and has no impact on component rerendering, just the logic below.
+  const currentHigh = useRef(100); //stored value and has no impact on component rerendering, just the logic below.
+
+  //destructing props below to use in useEffect. Pulling these props from outside, and givin them their own name; keeps track of changes.
+
+  const { userChoice, onGameOver } = props;
+
+  //useeffect determines if right choice/guess was made and if game was over.
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+      onGameOver(rounds);
+    }
+  }, [currentGuess, userChoice, onGameOver]);
+
+  const nextGuessHandler = (direction) => {
+    //validates to make sure correct options are applied given the current guess.
+    if (
+      (direction === 'lower' && currentGuess < props.userChoice) ||
+      (direction === 'greater' && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie!", 'You know that this is wrong...', [
+        { text: 'Sorry!', style: 'cancel' },
+      ]);
+      return; //stops function execution
+    }
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess; //stores computer guess as the new highest number.
+    } else {
+      currentLow.current = currentGuess; // stores the current guess, as the new lowestest boundary.ß
+    }
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+    setRounds((currentRounds) => currentRounds + 1);
+  };
+
   return (
     <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="LOWER" onPress={() => {}} />
-        <Button title="GREATER" onPress={() => {}} />
+        <Button title='LOWER' onPress={nextGuessHandler.bind(this, 'lower')} />
+        <Button
+          title='GREATER'
+          onPress={nextGuessHandler.bind(this, 'greater')}
+        />
       </Card>
     </View>
   );
@@ -37,14 +80,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: "center",
+    alignItems: 'center',
   },
   buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     marginTop: 20,
     width: 300,
-    maxWidth: "80%",
+    maxWidth: '80%',
   },
 });
 
